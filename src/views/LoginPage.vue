7c4a0ae78fca98288d55b02c4a92c777cedc78a6
poommin2543 <poommin2543@gmail.com>
2023-03-27 15:34:26 +0700

    fix login from

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,5 +1,41 @@
 <template>
-    <div id="login" class="main-box">
+    <v-app id="inspire">
+        <v-content>
+           <v-container fluid fill-height>
+              <v-layout align-center justify-center>
+                 <v-flex xs12 sm8 md4>
+                    <v-card class="elevation-12">
+                       <v-toolbar dark color="primary">
+                          <v-toolbar-title>Login form</v-toolbar-title>
+                       </v-toolbar>
+                       <v-card-text>
+                          <v-form>
+                             <v-text-field v-model="email"
+                                prepend-icon="person"
+                                name="login1"
+                                label="Login"
+                                type="text"
+                             ></v-text-field>
+                             <v-text-field v-model="password"
+                                id="password"
+                                prepend-icon="lock"
+                                name="password"
+                                label="Password"
+                                type="password"
+                             ></v-text-field>
+                          </v-form>
+                       </v-card-text>
+                       <v-card-actions>
+                          <v-spacer></v-spacer>
+                          <v-btn color="primary" @click="login">Login</v-btn>
+                       </v-card-actions>
+                    </v-card>
+                 </v-flex>
+              </v-layout>
+           </v-container>
+        </v-content>
+     </v-app>
+    <!-- <div id="login" class="main-box">
         <form @submit="loginWithEmail">
             <h1>Login</h1>
             <div class="title">
@@ -11,13 +47,11 @@
                 <input v-model="password" type="password" class="input" placeholder="Password">
             </div>
             <v-btn type="submit" value="submit" class="btn" @click="login">Login</v-btn>
-            <!-- <v-btn   @click="login">Login</v-btn> -->
         </form>
         <div>or Sign in with 3rd Party</div>
         <v-btn id="google" @click="loginWithProvider" class="btn-pic">
-            <!-- <img src="../assets/img/class logo.png" alt width="100px" height="100px"> -->
         </v-btn>
-    </div>
+    </div> -->
 </template>
 <script>
 import firebase from '@/plugins/firebase'
@@ -34,6 +68,8 @@ export default {
                 .signInWithEmailAndPassword(this.email, this.password)
                 .then(
                     user => {
+                        // this.$router.push("/home");
+                        // alert('Successfully login');
                         this.$router.replace("/home");
                         console.log(user)
                     },
@@ -60,3 +96,14 @@ export default {
     }
 }
 </script>
+<style>
+/* Sign in /Sign up /Reset password */
+
+.imgclassgo {
+    position: absolute;
+    width: 424px;
+    height: 457px;
+    left: 305px;
+    top: 312px;
+}
+</style>
